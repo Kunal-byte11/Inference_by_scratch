8645ebb4b02745c1c74b7cd4216b6920a696f5d1
kunal-byte11 <kunaldubey687@gmail.com>
2026-09-16 23:00:09 +0530

    Refactor code structure for improved readability and maintainability

---
+++ main.py
@@ -0,0 +1,17 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import os
+
+os.makedirs("../weights", exist_ok=True)
+
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+for name, param in model.named_parameters():
+    print(name, tuple(param.shape))
+
+    values = param.detach().cpu().numpy().flatten()
+
+    with open(f"../weights/{name}.txt", "w") as f:
+        f.write(" ".join(map(str, values)))
+
+tokenizer.save_pretrained("../weights/tokenizer")
